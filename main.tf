51d73689d60f94508cb0cbbea47253caf3a8a7b9
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-01-26 15:10:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "terrateam_bucket_03" {
+  bucket = "s03-tf-bucket"
+  acl    = "private"
+  tags = {
+   Name = "Saksham"
+ }
+}
